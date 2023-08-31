be0eb1f0186272b35f03d1e48c97a0f90d251125
brambleclaw1 <gavinhoeksema@gmail.com>
2023-08-31 16:39:33 -0600

    merge

--- src/main/java/net/mcreator/thehobbitmod/init/TheHobbitModModTabs.java
+++ src/main/java/net/mcreator/thehobbitmod/init/TheHobbitModModTabs.java
@@ -9,19 +9,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.CreativeModeTabEvent;
 
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TheHobbitModModTabs {
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(CreativeModeTabEvent.BuildContents tabData) {
-
-		if (tabData.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-		}
-	}
-
 	@SubscribeEvent
 	public static void buildTabContentsModded(CreativeModeTabEvent.Register event) {
 		event.registerCreativeModeTab(new ResourceLocation("the_hobbit_mod", "the_hobbit_mod"),
