d355777435614d2f9165cf11b339600423614858
brambleclaw1 <unconfigured@null.spigotmc.org>
2023-09-19 12:21:53 -0600

    wat

--- src/main/java/net/mcreator/thehobbitmod/item/SevenRingsItem.java
+++ src/main/java/net/mcreator/thehobbitmod/item/SevenRingsItem.java
@@ -17,7 +17,7 @@ public class SevenRingsItem extends Item {
 
 	@Override
 	public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
-		return 3F;
+		return 3f;
 	}
 
 	@Override
